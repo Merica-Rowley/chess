e40f4582945a3b55f14287962955542375d4aca9
Merica-Rowley <mericarowley@gmail.com>
2024-01-12 17:56:56 -0700

    finished adding functions from video demo

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -53,6 +53,6 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>()
+        return new ArrayList<>();
     }
 }
