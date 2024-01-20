6f3ab83c5073e0ae33202acfca16dfe3d38d26d8
Merica-Rowley <mericarowley@gmail.com>
2024-01-20 14:53:13 -0700

    implemented RookMovesCalculator; passes all RookMoveTests :boom:

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -71,6 +71,8 @@ public class ChessPiece {
                 possibleMoves = knightCalculator.pieceMoves();
                 break;
             case ROOK:
+                RookMovesCalculator rookCalculator = new RookMovesCalculator(board, myPosition);
+                possibleMoves = rookCalculator.pieceMoves();
                 break;
             case PAWN:
                 break;
