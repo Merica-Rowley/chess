25f028fcac5d75cd076c3f50599e08b7437752ff
Merica-Rowley <mericarowley@gmail.com>
2024-02-05 13:04:41 -0700

    fixed checkmate issues; passes 12/17 tests

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -52,11 +52,7 @@ public class ChessGame {
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         ChessPiece piece = board.getPiece(startPosition);
         Collection<ChessMove> validMoves = piece.pieceMoves(board, startPosition);
-        for (ChessMove move: validMoves) {
-            if (!testMove(move)) {
-                validMoves.remove(move);
-            }
-        }
+        validMoves.removeIf(move -> !testMove(move));
         return validMoves;
     }
 
