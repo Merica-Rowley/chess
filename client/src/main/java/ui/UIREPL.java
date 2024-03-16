ce0c87b735afb38cc3fb4fb8fcd2b388a5ac8310
Merica Rowley <mericarowley@gmail.com>
2024-03-15 23:05:25 -0600

    implemented postLogin in UIREPL

--- client/src/main/java/ui/UIREPL.java
+++ client/src/main/java/ui/UIREPL.java
@@ -1,5 +1,7 @@
 package ui;
 
+import chess.ChessGame;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Scanner;
@@ -59,7 +61,7 @@ public class UIREPL {
             else { // Default case called with "help" or any other input
                 System.out.println("\tregister <USERNAME> <PASSWORD> <EMAIL> - Create an account");
                 System.out.println("\tlogin <USERNAME> <PASSWORD> - Sign in");
-                System.out.println("\tquit - Exit program");
+                System.out.println("\tquit - Exit the program");
                 System.out.println("\thelp - View possible commands");
             }
         }
@@ -73,27 +75,72 @@ public class UIREPL {
             var input = line.split(" ");
 
             if (input[0].equals("create")) {
-
+                try {
+                    String response = facade.createGame(input[1]);
+                    System.out.println(response);
+                    if (response.contains("Error")) {
+                        System.out.print("Enter 'help' to see a list of commands\n");
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Error: missing input\nPlease enter a game name.");
+                }
             } else if (input[0].equals("list")) {
-
+                System.out.println(facade.listGames());
             } else if (input[0].equals("join")) {
-
+                try {
+                    // This would be true if a game id was included but not a team (in other words, the player will be an observer)
+                    String response;
+                    if (input.length == 2) {
+                        response = facade.joinGame(Integer.parseInt(input[1]), null);
+                    } else {
+                        response = facade.joinGame(Integer.parseInt(input[1]), ChessGame.TeamColor.valueOf(input[2]));
+                    }
+                    System.out.println(response);
+                    if (response.contains("Error")) {
+                        System.out.print("Enter 'help' to see a list of commands\n");
+                    } else {
+                        this.gameplay();
+                        break;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Error: missing input\nPlease enter a game id.");
+                } catch (NumberFormatException e) {
+                    System.out.println("Error: invalid game id\nPlease enter a valid game id.");
+                }
             } else if (input[0].equals("observe")) {
-
+                try {
+                    String response = facade.joinGame(Integer.parseInt(input[1]), null);
+                    System.out.println(response);
+                    if (response.contains("Error")) {
+                        System.out.print("Enter 'help' to see a list of commands\n");
+                    } else {
+                        this.gameplay();
+                        break;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Error: missing input\nPlease enter a game id.");
+                }
             } else if (input[0].equals("logout")) {
-
+                System.out.println(facade.logout());
+                this.preLogin();
+                break;
             } else if (input[0].equals("quit")) {
                 System.out.println("Logging out...");
                 facade.logout();
                 System.out.println("Goodbye!");
                 break;
             } else { // Default case called with "help" or any other input
-
-            }
+                System.out.println("\tcreate <NAME> - Create a game");
+                System.out.println("\tlist - See a list of games");
+                System.out.println("\tjoin <ID> [WHITE|BLACK|<empty>] - Join a game");
+                System.out.println("\tobserve <ID> - Observe a game");
+                System.out.println("\tlogout - Log out");
+                System.out.println("\tquit - Exit the program");
+                System.out.println("\thelp - View possible commands");            }
         }
     }
 
     private void gameplay() {
-
+        System.out.println("in gameplay");
     }
 }
